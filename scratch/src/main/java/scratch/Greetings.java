c7748c15e917e8305ec473c973694548bd26efd3
Kazik <Kazik@Kazik-VAIO>
2018-10-18 20:18:32 +0200

    Initial checking of maven project.

---
+++ scratch/src/main/java/scratch/Greetings.java
@@ -0,0 +1,12 @@
+package scratch;
+
+public class Greetings {
+
+	private static final String GREETING = "Hello";
+	
+	public String sayHello(String String) {
+		// TODO Auto-generated method stub
+		return GREETING + (String != null ? String: "Jonh Doe");
+	}
+
+}
---
+++ scratch/src/main/java/scratch/HelloWorld.java
@@ -0,0 +1,12 @@
+package scratch;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greetings greetings = new Greetings();
+		
+		System.out.println(greetings.sayHello(args.length>0?args[0]:null));
+	}
+
+}
